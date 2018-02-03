0fae800b831a1e48015ff8a48118ddec546d2408
EC2 Default User <ec2-user@ip-172-31-43-178.us-east-2.compute.internal>
2018-02-03 17:03:06 +0000

    rooting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
     
-    def hello
-        render html: "hello,world!!"
-    end
+  def hello
+    render html: "¡hola,mundo!!"
+  end
 
+  def goodbye
+    render html: "goodbye world!"
+  end
 end
